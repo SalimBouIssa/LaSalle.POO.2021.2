2eb6cc729c4a7a4a38ae7f46be705bebfc856c38
SalimBouIssa <89153302+SalimBouIssa@users.noreply.github.com>
2021-09-15 16:25:30 -0300

    Delete Trabalho Refatoração directory

--- Trabalho Refatoração/TesteEscalacao.java
+++
@@ -1,7 +0,0 @@
-public class TesteEscalacao {
-    public static void main (String[] args) {
-        Escalacao escalacao = new Escalacao();
-        escalacao.setEscalacao();
-        escalacao.imprimir();
-    }
-}
