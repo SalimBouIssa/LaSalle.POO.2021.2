90cd561e9b64294650de63729acd649a1b547d1f
SalimBouIssa <89153302+SalimBouIssa@users.noreply.github.com>
2021-09-15 16:27:00 -0300

    Add files via upload

---
+++ TrabalhoRefatoracao/TesteEscalacao.java
@@ -0,0 +1,7 @@
+public class TesteEscalacao {
+    public static void main (String[] args) {
+        Escalacao escalacao = new Escalacao();
+        escalacao.setEscalacao();
+        escalacao.imprimir();
+    }
+}
