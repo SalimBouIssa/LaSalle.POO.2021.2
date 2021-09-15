bf2dea2d8b2dd789a043e200ca94165358588c8f
SalimBouIssa <89153302+SalimBouIssa@users.noreply.github.com>
2021-09-15 16:22:55 -0300

    Add files via upload

---
+++ Trabalho Refatoração/Time.java
@@ -0,0 +1,29 @@
+public class Time {
+
+    // Atributos
+    private String nome;
+    private String escudo;
+    
+
+    // Métodos
+    // Set
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public void setEscudo (String escudo) {
+        this.escudo = escudo;
+    }
+
+
+    // Get
+    public String getNome () {
+        return this.nome;
+    }
+
+    public String getEscudo () {
+        return this.escudo;
+    }
+
+
+}
